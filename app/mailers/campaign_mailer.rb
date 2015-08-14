e49531101c1e577c8a626008950d3fe215ea656c
Dounan <dounan1@gmail.com>
2015-08-15 01:19:00 +0800

    added real campaign template (template1) to campaign_mailer erbs

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,8 +1,9 @@
 class CampaignMailer < ActionMailer::Base
   default from: (Rails.application.config.email_sender || "mike@perspectivo.com")
+  SUBJECT = (Rails.application.config.email_subject || 'Invitation to Perspectivo')
 
   def campaign_email(lead)
     @lead = lead
-    mail(to: @lead.email, subject: 'Sample Email')
+    mail(to: @lead.email, subject: SUBJECT)
   end
 end
