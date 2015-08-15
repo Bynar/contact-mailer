8f41e7e46491bc50c044157b05eb84a0e5cb1782
Dounan <dounan1@gmail.com>
2015-08-16 01:45:25 +0800

    autoload all files in lib folder

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module ContactMailerPrototype
 
     config.email_sender = ENV["EMAIL_SENDER"]
 
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
   end
 end
