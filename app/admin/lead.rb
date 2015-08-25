f67d51b2a1b4b071dc3e7b867305aa0fa1057a21
Dounan <dounan1@gmail.com>
2015-08-26 04:44:34 +0800

    added template scopes to leads in active_admin index

--- app/admin/lead.rb
+++ app/admin/lead.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register Lead do
   scope :unsent
 
+  scope :template_1 do |scope|
+    scope.with_template('template1')
+  end
+
+  scope :template_2 do |scope|
+    scope.with_template('template2')
+  end
 end
