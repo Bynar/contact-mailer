74639afcb9b9256bfe7f69bb3c2d6fd5c7477190
Dounan <dounan1@gmail.com>
2015-08-26 02:43:11 +0800

    customized active_admin dashboard to use branding

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,26 +6,25 @@ ActiveAdmin.register_page "Dashboard" do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
-    #
+
     # columns do
     #   column do
-    #     panel "Recent Posts" do
+    #     panel "Recent Leads" do
     #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
+    #         Lead.last(5).map do |lead|
+    #           li link_to(lead.email, admin_lead_path(lead))
     #         end
     #       end
     #     end
     #   end
-
+    #
     #   column do
     #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
+    #       para "Welcome to Perspectivo's Contacts Administration Page."
     #     end
     #   end
     # end
