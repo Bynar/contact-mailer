c489d8eb2eb19d9cba8fc08de6d13e81e9c75f76
Dounan <dounan1@gmail.com>
2015-08-27 03:35:13 +0800

    added database credentials as env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.postgres_user = ENV['POSTGRES_USER'] || 'postgres'
+  config.postgres_password = ENV['POSTGRES_PASSWORD'] || ''
 end
