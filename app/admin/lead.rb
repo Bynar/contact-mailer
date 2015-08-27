b31d7d2234a7b8dcd55c46370daf8bfa4de7688d
Dounan <dounan1@gmail.com>
2015-08-27 16:01:41 +0800

    added twitter_accounts scope to leads active_admin index

--- app/admin/lead.rb
+++ app/admin/lead.rb
@@ -8,4 +8,10 @@ ActiveAdmin.register Lead do
   scope :template_2 do |scope|
     scope.with_template('template2')
   end
+
+  scope :twitter_accounts do |scope|
+    scope.where('leads.id in (select max(leads.id) from leads
+                left join twitterers on leads.website = twitterers.real_url
+                group by leads.website) ')
+  end
 end
