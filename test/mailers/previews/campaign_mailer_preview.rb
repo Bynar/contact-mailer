f8f546f7a7253448f9ec415e18e29e7e079f1d3f
dounan <dounan1@gmail.com>
2015-09-17 19:04:32 +0800

    added template4 to email preview

--- test/mailers/previews/campaign_mailer_preview.rb
+++ test/mailers/previews/campaign_mailer_preview.rb
@@ -1,21 +1,27 @@
 # Preview all emails at http://localhost:3000/rails/mailers/campaign_mailer
 class CampaignMailerPreview < ActionMailer::Preview
 
-  def campaign_email1
+  def template1
     @lead = Lead.new(first_name: 'tester', email: 'tester@example.com')
 
     CampaignMailer.campaign_email(@lead, 'template1')
   end
 
-  def campaign_email2
+  def template2
     @lead = Lead.new(first_name: 'tester', email: 'tester@example.com')
 
     CampaignMailer.campaign_email(@lead, 'template2')
   end
 
-  def campaign_email3
+  def template3
     @lead = Lead.new(first_name: 'tester', email: 'tester@example.com')
 
     CampaignMailer.campaign_email(@lead, 'template3')
+  end
+
+  def template4
+    @lead = Lead.new(first_name: 'tester', email: 'tester@example.com')
+
+    CampaignMailer.campaign_email(@lead, 'template4')
   end
 end
