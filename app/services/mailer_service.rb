7404c28ffde6313581c1432fba0b9a2684e47920
dounan <dounan1@gmail.com>
2015-09-22 22:21:15 +0800

    added mixpanel tracker to mailer_service

--- app/services/mailer_service.rb
+++ app/services/mailer_service.rb
@@ -1,6 +1,9 @@
 class MailerService
 
   def self.send(size, template, mailer)
+
+    tracker = Mixpanel::Tracker.new(Rails.application.config.mixpanel_api)
+
     Lead.unsent.with_template(template).first(size).each do |l|
       begin
         mailer.new.perform(l, template)
@@ -11,7 +14,21 @@ class MailerService
         p 'template ' + template +' not found'
       else
         l.update_attributes(mandrill_sent_date: Time.current)
+        track(tracker, l, template)
       end
     end
+  end
+
+  def self.track(tracker, lead, template)
+    # Track an event on behalf of user "User1"
+    tracker.track(lead.email, 'Invitation Sent', template: template)
+
+    # Send an update to User1's profile
+    tracker.people.set(lead.email, {
+                                  '$first_name' => lead.first_name,
+                                  '$last_name' => lead.last_name,
+                                  '$email' => lead.email,
+                                  website: lead.website
+                              })
   end
 end
