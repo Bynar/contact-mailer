cc22651593b8d6c6a5bf1d55ee5e21a38f153d7f
dounan <dounan1@gmail.com>
2015-10-30 01:25:47 +0800

    added subject variations to campaign_mailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,11 +1,18 @@
 class CampaignMailer < ActionMailer::Base
   default from: (Rails.application.config.email_sender || "mike@perspectivo.com")
   SUBJECT = (Rails.application.config.email_subject || 'Invitation to Perspectivo')
+  S2 = "Invitation to check out Perspectivo. It's different but it's cool."
 
   def campaign_email(lead, template_name)
     @lead = lead
     if !@lead.unsent? then raise Exceptions::MailerAddressConflict, "#{@lead.email} already sent to previously" end
 
-    mail(to: @lead.email, subject: SUBJECT, template_name: template_name)
+    mail(to: @lead.email, subject: subject(template_name), template_name: template_name)
+  end
+
+private
+
+  def subject(template_name)
+    (template_name.match(/S2/)).nil? ? SUBJECT : S2
   end
 end
