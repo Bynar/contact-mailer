e62e3324fc9d0615edc4e19208eefd183bb5a963
dounan <dounan1@gmail.com>
2015-11-30 20:08:54 +0800

    added counts grouped by dates to Contact and Twitterer models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,12 @@ class Contact < ActiveRecord::Base
                  .having("count(leads.id)=0 and count(contacts.email) > 0")
                  .maximum(:full_name) }
 
+  scope :by_created_dates, -> { group("CAST(created_at as DATE)").order("CAST(created_at as DATE)") }
+
+  def self.created
+    self.by_created_dates.count
+  end
+
   def first_name
     NameService.first_name(full_name)
   end
--- app/models/twitterer.rb
+++ app/models/twitterer.rb
@@ -1,7 +1,17 @@
 class Twitterer < ActiveRecord::Base
   scope :not_crawled, -> { where(crawled_at: nil) }
+  scope :by_crawled_dates, -> { group("CAST(crawled_at as DATE)").order("CAST(crawled_at as DATE)") }
+  scope :by_created_dates, -> { group("CAST(created_at as DATE)").order("CAST(created_at as DATE)") }
 
   def self.outstanding
     self.not_crawled.count
+  end
+
+  def self.crawled
+    self.by_crawled_dates.count
+  end
+
+  def self.processing
+    self.not_crawled.by_created_dates.count
   end
 end
